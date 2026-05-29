a6ebe9e62713fb757985b566a6011be466542a20
dverdini <dverdini@f4r11s12.paris.42.school>
2026-05-29 12:11:28 +0200

    add working pipe execution for two commands

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: dverdini <dverdini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/23 16:36:08 by dverdini          #+#    #+#             */
-/*   Updated: 2026/05/25 15:05:47 by dverdini         ###   ########.fr       */
+/*   Updated: 2026/05/29 12:08:34 by dverdini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,91 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <stdlib.h>
+
+/*
+pms_execute_pipe()
+{
+	creo pipe fd[2]
+
+	fork primo child:
+		dup2(fd[1], STDOUT_FILENO)
+		close fd[0]
+		close fd[1]
+		eseguo cmd1
+
+	fork second child:
+		dup2(fd[0], STDIN_FILENO)
+		close fd[0]
+		close fd[1]
+		eseguo cmd2
+
+	parent:
+		closefd[0]
+		close fd[1]
+		waitpid child1
+		waitpid child2		
+}
+*/
+
+void	ms_execute_in_child(t_cmd *cmd, char **envp, t_env **env)
+{
+	char	*path;
+
+	if (!cmd || !cmd->argv || !cmd->argv[0])
+		exit(EXIT_SUCCESS);
+	if (is_builtin(cmd->argv[0]))
+	{
+		run_builtin(cmd, env);
+		exit(EXIT_SUCCESS);
+	}
+	path = ms_find_cmd_path(cmd->argv[0], envp);
+	if (!path)
+	{
+		ft_printf("%s: command not found\n", cmd->argv[0]);
+		exit(127);
+	}
+	execve(path, cmd->argv, envp);
+	perror("execve");
+	free(path);
+	exit(EXIT_FAILURE);
+}
+
+void	ms_execute_pipe(t_cmd *cmd, char **envp, t_env **env)
+{
+	int	fd[2];
+	pid_t	pid1;
+	pid_t	pid2;
+
+	if (pipe(fd) < 0)
+		return ;
+	pid1 = fork();
+	if (pid1 == 0)
+	{
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		//ft_printf("child1\n");
+		//ms_execute_single_cmd(cmd, envp, env);
+		ms_execute_in_child(cmd, envp, env);
+		exit(EXIT_SUCCESS);
+	}
+	pid2 = fork();
+	if (pid2 == 0)
+	{
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		//ft_printf("child2\n");
+		//ms_execute_single_cmd(cmd->next, envp, env);
+		ms_execute_in_child(cmd->next, envp, env);
+		exit(EXIT_SUCCESS);
+	}
+	close(fd[0]);
+	close(fd[1]);
+
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
+}
 
 void	ms_run_external(t_cmd *cmd, char **envp)
 {
@@ -31,11 +116,11 @@ void	ms_run_external(t_cmd *cmd, char **envp)
 		ft_printf("%s: command not found\n", cmd->argv[0]);
 		return ;
 	}
-	// --- DEBUG MSG-----------------
-	ft_printf("#=======================================================\n");
-	ft_printf("external command launched\n");
-	ft_printf("#=======================================================\n");
-	// ------------------------------
+//	// --- DEBUG MSG-----------------
+//	ft_printf("#=======================================================\n");
+//	ft_printf("external command launched\n");
+//	ft_printf("#=======================================================\n");
+//	// ------------------------------
 	pid = fork();
 	if (pid == 0)
 	{
@@ -57,6 +142,22 @@ void	ms_run_external(t_cmd *cmd, char **envp)
 				exit(1);
 			}
 			dup2(fd, STDOUT_FILENO);// --- ADD DEBUG: if (... < 0)
+			close(fd);
+		}
+		if (cmd->infile)
+		{
+			fd = open(cmd->infile, O_RDONLY);
+			if (fd < 0)
+			{
+				perror("open");
+				exit(EXIT_FAILURE);
+			}
+			if (dup2(fd, STDIN_FILENO) < 0)
+			{
+				perror("dup2");
+				close(fd);
+				exit(EXIT_FAILURE);
+			}
 			close(fd);
 		}
 		execve(path, cmd->argv, envp);
@@ -110,13 +211,25 @@ void	ms_execute_single_cmd(t_cmd *cmd, char **envp, t_env **env)
 void	ms_executor(t_cmd *cmd, char **envp, t_env **env)
 {
 	// --- DEBUG MSG-----------------
-	ft_printf("#=======================================================\n");
-	ft_printf("executor launched\n");
-	ft_printf("#=======================================================\n");
+//	ft_printf("#=======================================================\n");
+//	ft_printf("executor launched\n");
+//	ft_printf("#=======================================================\n");
 	// ------------------------------
-	while (cmd)
+	if (!cmd)
+		return ;
+	if (cmd->next)
 	{
-		ms_execute_single_cmd(cmd, envp, env);
+		ms_execute_pipe(cmd, envp, env);
+		return ;
+	}
+	ms_execute_single_cmd(cmd, envp, env);
+/*	while (cmd)
+	{
+		if (cmd && cmd->next)
+			ms_execute_pipe(cmd, envp, env);
+		else
+			ms_execute_single_cmd(cmd, envp, env);
 		cmd = cmd->next;
 	}
+*/
 }
